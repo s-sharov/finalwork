07921f3289dcb3a364c889d1bbe80784725524f0
Sergey Sharov <s.sharov@stud.rksi.ru>
2023-02-13 22:20:38 +0300

    Add work c#

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"Данная", "программа", "должна", "вывести", "на", "экран", "массив", "в", "котором", "будет", "содержаться"
+    , "элементы", "данного", "масива", "количество", "символов", "которых", "не", "превышает", "трех"};
+
+
+string[] newArray = CreateStringArray(array);
+Console.WriteLine("Имеется исходный массив: ");
+PrintStringArray(array);
+Console.WriteLine();
+Console.WriteLine("Все элементы исходного массива, в котором символов меньше или равно 3:  ");
+Console.WriteLine();
+PrintStringArray(newArray);
+
+string[] CreateStringArray(string[] oldArray)
+{
+    string[] arr = new string[oldArray.Length];
+    int count = 0;
+    for (int i = 0; i < oldArray.Length; i++)
+    {
+        if(oldArray[i].Length <= 3 )
+        {
+            arr[count] = oldArray[i];
+            count++;
+        }
+    }
+    return arr;
+}
+
+void PrintStringArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($" {arr[i]} ");
+    }
+}
